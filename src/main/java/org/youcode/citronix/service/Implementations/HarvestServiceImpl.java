eae04f414cd33ab86b5212796c2704be45a49e7a
ibtissam el hani <ibtissamelhani36@gmail.com>
2024-11-19 21:41:32 +0100

    create harvest added

--- src/main/java/org/youcode/citronix/service/Implementations/HarvestServiceImpl.java
+++ src/main/java/org/youcode/citronix/service/Implementations/HarvestServiceImpl.java
@@ -2,12 +2,18 @@ package org.youcode.citronix.service.Implementations;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.youcode.citronix.domain.entities.Field;
 import org.youcode.citronix.domain.entities.Harvest;
+import org.youcode.citronix.domain.entities.HarvestDetail;
+import org.youcode.citronix.domain.entities.Tree;
+import org.youcode.citronix.repository.HarvestDetailRepository;
 import org.youcode.citronix.repository.HarvestRepository;
 import org.youcode.citronix.service.FieldService;
 import org.youcode.citronix.service.HarvestService;
+import org.youcode.citronix.web.exception.Harvest.HarvestAlreadyExistException;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -16,11 +22,41 @@ public class HarvestServiceImpl implements HarvestService {
 
     private final HarvestRepository harvestRepository;
     private final FieldService fieldService;
+    private final HarvestDetailRepository harvestDetailRepository;
 
+    @Transactional
     @Override
     public Harvest createHarvest(UUID fieldId, Harvest harvest) {
 
         Field fieldToHarvest = fieldService.getFieldById(fieldId);
+        boolean exist = harvestRepository.existsByFieldAndSeason(fieldToHarvest,harvest.getSeason());
+        if (exist){
+            throw new HarvestAlreadyExistException("A harvest already exists for this field and season.");
+        }
 
+        List<Tree> trees = fieldToHarvest.getTrees();
+
+        double totalQuantity = trees.stream()
+                .mapToDouble(Tree::getProductivity)
+                .sum();
+
+        Harvest harvestToSave = Harvest.builder()
+                .field(fieldToHarvest)
+                .date(harvest.getDate())
+                .season(harvest.getSeason())
+                .totalQuantity(totalQuantity)
+                .build();
+
+        Harvest savedHarvest = harvestRepository.save(harvestToSave);
+
+        List<HarvestDetail> harvestDetails = trees.stream()
+                .map(tree -> HarvestDetail.builder()
+                        .harvest(savedHarvest)
+                        .tree(tree)
+                        .quantity(tree.getProductivity())
+                        .build())
+                .toList();
+        harvestDetailRepository.saveAll(harvestDetails);
+        return savedHarvest;
     }
 }
