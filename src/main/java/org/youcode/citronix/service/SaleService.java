2647f9388b932491209fc411db89cadf81ace87b
ibtissam el hani <ibtissamelhani36@gmail.com>
2024-11-21 12:10:54 +0100

    map to sale response VM

--- src/main/java/org/youcode/citronix/service/SaleService.java
+++ src/main/java/org/youcode/citronix/service/SaleService.java
@@ -1,12 +1,11 @@
 package org.youcode.citronix.service;
 
-import org.youcode.citronix.DTO.SaleDTO;
 import org.youcode.citronix.domain.entities.Sale;
 
 import java.util.UUID;
 
 public interface SaleService {
-    SaleDTO createSale(UUID harvestId, Sale sale);
+    Sale createSale(UUID harvestId, Sale sale);
     void deleteSale(UUID saleId);
     Sale findById(UUID saleId);
 }
