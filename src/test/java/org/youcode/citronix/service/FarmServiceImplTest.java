6a5367e8e8eaf129660edb6c8d8f5c0aba814243
ibtissam el hani <ibtissamelhani36@gmail.com>
2024-11-21 23:37:02 +0100

    method addField in fieldService tested

--- src/test/java/org/youcode/citronix/service/FarmServiceImplTest.java
+++ src/test/java/org/youcode/citronix/service/FarmServiceImplTest.java
@@ -2,12 +2,9 @@ package org.youcode.citronix.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
