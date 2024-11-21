9e819cafa9b98e2bfaccfd73d834b1551bb67762
ibtissam el hani <ibtissamelhani36@gmail.com>
2024-11-21 18:08:16 +0100

    unit test for createFarm & getALlFarms done

--- src/test/java/org/youcode/citronix/service/FarmServiceImplTest.java
+++ src/test/java/org/youcode/citronix/service/FarmServiceImplTest.java
@@ -6,17 +6,18 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.youcode.citronix.domain.entities.Farm;
+import org.youcode.citronix.exception.Farm.InvalidFarmException;
 import org.youcode.citronix.repository.FarmRepository;
 import org.youcode.citronix.repository.FarmSearchRepository;
 import org.youcode.citronix.service.Implementations.FarmServiceImpl;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class FarmServiceImplTest {
 
@@ -69,6 +70,47 @@ class FarmServiceImplTest {
         verify(farmRepository).save(farm);
     }
 
+    @Test
+    void FarmService_save_throwsInvalidFarmException() {
+
+        //When & Then
+        assertThrows(InvalidFarmException.class,
+                ()-> farmService.save(null)
+        );
+        verify(farmRepository,never()).save(any(Farm.class));
+    }
+
+    @Test
+    void FarmService_getAllFarms_returnsAllFarm() {
+
+        //Given
+        Farm farm1 = Farm.builder()
+                .name("name 1")
+                .area(200)
+                .creationDate(LocalDate.parse("2024-01-01"))
+                .location("location 1")
+                .build();
+
+        Farm farm2 = Farm.builder()
+                .name("name 2")
+                .area(200)
+                .creationDate(LocalDate.parse("2024-03-03"))
+                .location("location 2")
+                .build();
+
+        List<Farm> expectedList = List.of(farm1,farm2);
+
+        //When
+
+        when(farmRepository.findAll()).thenReturn(expectedList);
+        List<Farm> resultedList = farmService.getAllFarms();
+
+        //Then
+        verify(farmRepository).findAll();
+        assertEquals(2,resultedList.size());
+        assertEquals(expectedList,resultedList);
+    }
 
 
-}
+
+    }
